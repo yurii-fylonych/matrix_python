8edd15f755451b15b23a3cd8bdcfca86521bf3d3
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-21 09:08:46 +0300

    Second task

---
+++ array_problem_2_2.py
@@ -0,0 +1,19 @@
+array = []
+
+N = int(input('Enter number of rows = '))
+M = int(input('Enter number of columns = '))
+
+for row in range(N):
+    array.append([])
+    for column in range(M):
+        array[row].append(random.randint(-20, 25))
+
+for i in range(N):
+    sum = 0
+    for j in range(M):
+        sum += array[i][j]
+    print('Sum in ', i, ' row is ', sum)
+
+
+for rows in array:
+    print(rows)

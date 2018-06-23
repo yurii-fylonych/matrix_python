dc6cc612ff08345d0e8d89aa5dfd44c6a611c4df
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-23 09:25:55 +0300

    Task 16

---
+++ array_problem_2_16.py
@@ -0,0 +1,13 @@
+array = []
+
+N = int(input('Enter number of rows = '))
+M = int(input('Enter number of columns = '))
+
+for row in range(N):
+    array.append([])
+    for column in range(M):
+        array[row].append(row + 1)
+
+
+for row in array:
+    print(row)
